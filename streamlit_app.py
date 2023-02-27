2518766e62f27f85b0f30a68af6ddea80b18d4a0
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-02-27 15:24:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,6 +72,36 @@ st.pyplot(fig1)
 
 
 
+
+
+
+from geopy.geocoders import Nominatim
+
+# Load the data
+df = pd.read_csv('df_final.csv')
+
+# Clean and transform the data
+geolocator = Nominatim(user_agent="my_app")
+latitudes = []
+longitudes = []
+for country in df['Country']:
+    location = geolocator.geocode(country)
+    latitudes.append(location.latitude)
+    longitudes.append(location.longitude)
+
+df['Latitude'] = latitudes
+df['Longitude'] = longitudes
+
+# Create the map
+fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude', hover_name='Country', hover_data=['Happiness_Rank', 'Happiness_Score'], color='Happiness_Score', size='Economy', zoom=1, height=500)
+fig.update_layout(mapbox_style='carto-positron')
+
+# Build the Streamlit app
+st.title('Happiness Index 2017 Map')
+st.plotly_chart(fig)
+
+
+
 # import geopy
 # import folium
 # from geopy.geocoders import Nominatim
